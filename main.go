02b7e91d3868f46629079002dbbdd07b28a85bd5
synth-evol <ccroft323@gmail.com>
2024-05-16 22:44:56 -0600

    Update to port 8000

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func ProcessForm(c *fiber.Ctx) error {
 			"body":"Your tale was lost in the Astral Sea! Try again later!"
 		}`)
 	}
-	http.Post("http://127.0.0.1:3000/", "application/json", bytes.NewBuffer(messageJson))
+	http.Post("http://127.0.0.1:8000/", "application/json", bytes.NewBuffer(messageJson))
 	return c.Render("greeting", fiber.Map{"Greeting": confirmation})
 }
 
