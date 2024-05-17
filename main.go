7c1af952a42a54f695e419a9e469a122904381a8
synth-evol <ccroft323@gmail.com>
2024-05-16 23:30:58 -0600

    Add http

--- main.go
+++ main.go
@@ -34,7 +34,7 @@ func ProcessForm(c *fiber.Ctx) error {
 			"body":"Your tale was lost in the Astral Sea! Try again later!"
 		}`)
 	}
-	http.Post("anon-bot-messager:8000/", "application/json", bytes.NewBuffer(messageJson))
+	http.Post("http://anon-bot-messager:8000/", "application/json", bytes.NewBuffer(messageJson))
 	return c.Render("greeting", fiber.Map{"Greeting": confirmation})
 }
 
